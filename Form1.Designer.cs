60756c4d8ffafd0c6f7eccba3d38be13d8113d85
Luka <36271279+NotJ3st3r@users.noreply.github.com>
2020-02-21 20:01:18 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace MouseMovement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl_pos = new System.Windows.Forms.Label();
+            this.tmr_mouse = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbl_pos
+            // 
+            this.lbl_pos.AutoSize = true;
+            this.lbl_pos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbl_pos.Location = new System.Drawing.Point(12, 9);
+            this.lbl_pos.Name = "lbl_pos";
+            this.lbl_pos.Size = new System.Drawing.Size(46, 17);
+            this.lbl_pos.TabIndex = 0;
+            this.lbl_pos.Text = "label1";
+            // 
+            // tmr_mouse
+            // 
+            this.tmr_mouse.Enabled = true;
+            this.tmr_mouse.Interval = 1;
+            this.tmr_mouse.Tick += new System.EventHandler(this.tmr_mouse_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 38);
+            this.Controls.Add(this.lbl_pos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_pos;
+        private System.Windows.Forms.Timer tmr_mouse;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MouseMovement
+{
+    public partial class Form1 : Form
+    {
+        Random RDM = new Random();
+        Stopwatch sw = new Stopwatch();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sw.Start();
+        }
+
+        private void tmr_mouse_Tick(object sender, global::System.EventArgs e)
+        {
+            //lbl_pos.Text = @"x-pos: " + global::System.Windows.Forms.Cursor.Position.X + @"     y-pos: " + global::System.Windows.Forms.Cursor.Position.Y;
+            Cursor.Position = new Point(Cursor.Position.X + (RDM.Next() % 3 - 1) * (RDM.Next() % ((int)sw.Elapsed.Seconds*1000 / 2200 + 2)),
+                Cursor.Position.Y + (RDM.Next() % 3 - 1) * (RDM.Next() % ((int)sw.Elapsed.Seconds * 1000 / 2200 + 2)));
+            lbl_pos.Text = Convert.ToString(sw.Elapsed.Seconds);
+            //Cursor.Position = new Point(Cursor.Position.X + RDM.Next(-1, 2), Cursor.Position.Y + RDM.Next(-1, 2));
+        }
+    }
+}
+
+//SetCursorPos(cursor.x + (random() % 3 - 1) * (random() % (runtime / 2200 + 2)), cursor.y + (random() % 3 - 1) * (random() % (runtime / 2200 + 2)));
